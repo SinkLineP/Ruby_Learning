7073bc8995232ac27732e54495c374f7781f85d4
SinkLine_P <popovartem505@gmail.com>
2023-02-06 19:08:42 +0300

    search YAML file

--- Lesson_06/index.rb
+++ Lesson_06/index.rb
@@ -1,9 +1,16 @@
+require "yaml"
+
 # Ввести имя с клавиатуры
 
 print "Enter your name: "
 username = gets.strip
 
 # Откуда-то взять вопросы и ответы
+# YAML
+
+data = YAML.safe_load_file("quetion.yml", symbolize_names: true) #читаем файл
+puts data.inspect
+
 # Брать каждый вопрос по очереди и предлагать 4 варинта ответа
 # Юзер вводит ответ с клавиатуры
 # Мы сравниваем ответ с клавиатуры
