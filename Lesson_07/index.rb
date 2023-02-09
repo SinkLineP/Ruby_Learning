6ae7a3e993cce03c0ec8a0bb104e742c9001f7b9
SinkLine_P <popovartem505@gmail.com>
2023-02-09 19:33:31 +0300

    add method Age to class Animal

--- Lesson_07/index.rb
+++ Lesson_07/index.rb
@@ -9,14 +9,21 @@ class Animal
     # Тут мы описываем какие атрибуты есть у животных
     # и ещё, какие методы (функции) они могут реализовать 
 
-    def initialize(name) # метод, который вызывается при создании нового животного
+    def initialize(name, age) # метод, который вызывается при создании нового животного
         # name - это переменная (аргумент метода), которая существует только пока работает метод
         # puts name # name - это локальная переменная
         # сохраняем его ЛИЧНОЕ имя
-        name = name # переменная образца класса instance variables
+        @name = name # переменная образца класса instance variables
+        @age = age
+    end
+
+    def age # это метод
+        puts "Мне #{@age} лет"
     end
 end
 
 # Порождаем образец (экземпляр) этого класса
-Animal.new("Kotopes") # создаём новое животное и запускаем метод initialize
-Animal.new("Pesokot")
+animal1 = Animal.new("Kotopes", 3) # создаём новое животное и запускаем метод initialize
+animal2 = Animal.new("Pesokot", 2)
+
+puts animal1.age
