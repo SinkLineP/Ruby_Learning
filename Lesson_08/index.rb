741bc118b761e8805a8e9b35ef1d2bff929f4d80
SinkLine_P <popovartem505@gmail.com>
2023-02-15 12:30:42 +0300

    learning method (attr)

---
+++ Lesson_08/index.rb
@@ -0,0 +1,12 @@
+class Animal 
+    attr_accessor :age # создание метода age
+    attr_reader :name # метод только для чтения!
+    attr_writer :name # метод для изменения!
+
+    def initialize(name, age) # метод, который вызывается при создании нового животного
+        @name = name # name - это переменная (аргумент метода), существует пока работает метод
+        @age = age 
+    end
+end
+
+animal1 = Animal.new("Kotopes", 3) # создаем новое животное и запускаем метод initialize
